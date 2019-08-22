113c48b6e54b8a50ed53656c90a508fcd3570821
Scott Appleton <scottappleton09@gmail.com>
2019-08-21 18:15:44 -0700

    added some information to admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,10 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
 
+  
   def show
-    
+    @orders = Order.all.count
+    @categories = Category.all.count
   end
 
 end
